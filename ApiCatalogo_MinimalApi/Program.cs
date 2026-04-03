7ea1a4b96f98c83cf398b3e19461d4691e890d93
chmov <chmov@PC-CARLOSHMV>
2026-04-02 21:33:56 -0300

    Refatorar ajuste Program e implementar ServiceCollectionExtensions

--- ApiCatalogo_MinimalApi/Program.cs
+++ ApiCatalogo_MinimalApi/Program.cs
@@ -1,79 +1,14 @@
+using ApiCatalogo.ApiEndpoints;
 using ApiCatalogo.AppServicesExtensions;
-using ApiCatalogo.Services;
 using ApiCatalogo_MinimalApi.ApiEndpoints;
-using ApiCatalogo_MinimalApi.Context;
-using ApiCatalogo_MinimalApi.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.//ConfigureServices
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCatalogo", Version = "v1" });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = @"JWT Authorization header using the Bearer scheme.
-                    Enter 'Bearer'[space].Example: \'Bearer 12345abcdef\'",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                          new OpenApiSecurityScheme
-                          {
-                              Reference = new OpenApiReference
-                              {
-                                  Type = ReferenceType.SecurityScheme,
-                                  Id = "Bearer"
-                              }
-                          },
-                         new string[] {}
-                    }
-                });
-});
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-                 options
-                 .UseMySql(connectionString,
-                 ServerVersion.AutoDetect(connectionString)));
-
-
-builder.Services.AddSingleton<ITokenService>(new TokenService());
-
-builder.Services.AddAuthentication
-                 (JwtBearerDefaults.AuthenticationScheme)
-                 .AddJwtBearer(options =>
-                 {
-                     options.TokenValidationParameters = new TokenValidationParameters
-                     {
-                         ValidateIssuer = true,
-                         ValidateAudience = true,
-                         ValidateLifetime = true,
-                         ValidateIssuerSigningKey = true,
-
-                         ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                         ValidAudience = builder.Configuration["Jwt:Audience"],
-                         IssuerSigningKey = new SymmetricSecurityKey
-                         (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-                     };
-                 });
-
-builder.Services.AddAuthorization();
-
+builder.AddApiSwagger();
+builder.AddPersistence();
+builder.Services.AddCors();
+builder.AddAutenticationJwt();
 
 var app = builder.Build();
 
@@ -81,10 +16,7 @@ app.MapAutenticacaoEndpoints();
 app.MapCategoriasEndpoints();
 app.MapProdutosEndpoints();
 
-
-
 var environment = app.Environment;
-
 app.UseExceptionHandling(environment)
     .UseSwaggerMiddleware()
     .UseAppCors();
@@ -93,4 +25,3 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.Run();
-
